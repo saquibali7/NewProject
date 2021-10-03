978d5ca4411108943a0c227928d07a55a5fc1c39
saquib <saquibali534@gmail.com>
2021-10-03 14:09:25 +0530

    form added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -9,6 +10,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb://localhost:27017/registrationForm');
+
+const formSchema = {
+  FullName: String,
+  Email: String,
+  MoNumber: String,
+  Address: String
+};
+
+const Form = mongoose.model("Form", formSchema);
 
 
 app.get("/", function(req, res){
@@ -20,8 +31,24 @@ app.get("/donate", function(req, res){
 });
 
 
+app.get("/join", function(req, res){
+  res.render("join");
+});
 
+app.post("/join", function(req, res) {
+  const form = new Form({
+    FullName: req.body.fullName,
+    Email: req.body.email,
+    MoNumber: req.body.moNumber,
+    Address: req.body.address
+  });
 
+  form.save(function(err) {
+    if(!err) {
+      res.redirect("/");
+    }
+  });
+});
 
 let port = process.env.PORT;
 if (port == null || port == "") {
